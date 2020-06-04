a469a18de8b554aeda2e8ad383d5db19759b0f48
KB <jonathanaleman@292gmail.com>
2020-06-04 01:42:32 -0600

    Se termino el trabajo

--- Todo lo visto/Todo lo visto/Form1.Designer.vb
+++ Todo lo visto/Todo lo visto/Form1.Designer.vb
@@ -22,30 +22,94 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.Fecha = New System.Windows.Forms.Label()
+        Me.mkFecha = New System.Windows.Forms.MaskedTextBox()
+        Me.cbProductos = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtProducto = New System.Windows.Forms.TextBox()
-        Me.txtCantidad = New System.Windows.Forms.TextBox()
         Me.btnCalcular = New System.Windows.Forms.Button()
-        Me.btnSalir = New System.Windows.Forms.Button()
         Me.chkDescuento = New System.Windows.Forms.CheckBox()
         Me.chkISV = New System.Windows.Forms.CheckBox()
+        Me.cbDescuento = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtISV = New System.Windows.Forms.TextBox()
+        Me.txtDescuento = New System.Windows.Forms.TextBox()
+        Me.txtSubtotal = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.NuevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuOpciones = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuNuevo = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuSalir = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnPagar = New System.Windows.Forms.Button()
+        Me.btnPedido = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.txtCantidad)
-        Me.GroupBox1.Controls.Add(Me.txtProducto)
+        Me.GroupBox1.Controls.Add(Me.Fecha)
+        Me.GroupBox1.Controls.Add(Me.mkFecha)
+        Me.GroupBox1.Controls.Add(Me.cbProductos)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 39)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(211, 118)
+        Me.GroupBox1.Size = New System.Drawing.Size(234, 163)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Productos"
+        Me.GroupBox1.Text = "Pedido"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(84, 73)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(121, 22)
+        Me.txtCantidad.TabIndex = 8
+        '
+        'Fecha
+        '
+        Me.Fecha.AutoSize = True
+        Me.Fecha.Location = New System.Drawing.Point(6, 113)
+        Me.Fecha.Name = "Fecha"
+        Me.Fecha.Size = New System.Drawing.Size(47, 17)
+        Me.Fecha.TabIndex = 7
+        Me.Fecha.Text = "Fecha"
+        '
+        'mkFecha
+        '
+        Me.mkFecha.Location = New System.Drawing.Point(84, 110)
+        Me.mkFecha.Mask = "00/00/0000"
+        Me.mkFecha.Name = "mkFecha"
+        Me.mkFecha.Size = New System.Drawing.Size(121, 22)
+        Me.mkFecha.TabIndex = 6
+        Me.mkFecha.ValidatingType = GetType(Date)
+        '
+        'cbProductos
+        '
+        Me.cbProductos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbProductos.FormattingEnabled = True
+        Me.cbProductos.Items.AddRange(New Object() {"Camisetas", "Tenis", "Jeans", "Shorts", "Gorras", "Perfumes"})
+        Me.cbProductos.Location = New System.Drawing.Point(84, 37)
+        Me.cbProductos.Name = "cbProductos"
+        Me.cbProductos.Size = New System.Drawing.Size(121, 24)
+        Me.cbProductos.TabIndex = 5
         '
         'Label2
         '
@@ -61,46 +125,23 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(6, 37)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 17)
+        Me.Label1.Size = New System.Drawing.Size(72, 17)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Precio"
-        '
-        'txtProducto
-        '
-        Me.txtProducto.Location = New System.Drawing.Point(90, 37)
-        Me.txtProducto.Name = "txtProducto"
-        Me.txtProducto.Size = New System.Drawing.Size(100, 22)
-        Me.txtProducto.TabIndex = 2
-        '
-        'txtCantidad
-        '
-        Me.txtCantidad.Location = New System.Drawing.Point(90, 72)
-        Me.txtCantidad.Name = "txtCantidad"
-        Me.txtCantidad.Size = New System.Drawing.Size(100, 22)
-        Me.txtCantidad.TabIndex = 3
+        Me.Label1.Text = "Productos"
         '
         'btnCalcular
         '
-        Me.btnCalcular.Location = New System.Drawing.Point(245, 70)
+        Me.btnCalcular.Location = New System.Drawing.Point(279, 111)
         Me.btnCalcular.Name = "btnCalcular"
         Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
         Me.btnCalcular.TabIndex = 1
         Me.btnCalcular.Text = "Calcular"
         Me.btnCalcular.UseVisualStyleBackColor = True
         '
-        'btnSalir
-        '
-        Me.btnSalir.Location = New System.Drawing.Point(245, 111)
-        Me.btnSalir.Name = "btnSalir"
-        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
-        Me.btnSalir.TabIndex = 2
-        Me.btnSalir.Text = "Salir"
-        Me.btnSalir.UseVisualStyleBackColor = True
-        '
         'chkDescuento
         '
         Me.chkDescuento.AutoSize = True
-        Me.chkDescuento.Location = New System.Drawing.Point(12, 174)
+        Me.chkDescuento.Location = New System.Drawing.Point(12, 226)
         Me.chkDescuento.Name = "chkDescuento"
         Me.chkDescuento.Size = New System.Drawing.Size(98, 21)
         Me.chkDescuento.TabIndex = 3
@@ -110,27 +151,203 @@ Partial Class Form1
         'chkISV
         '
         Me.chkISV.AutoSize = True
-        Me.chkISV.Location = New System.Drawing.Point(125, 174)
+        Me.chkISV.Location = New System.Drawing.Point(140, 226)
         Me.chkISV.Name = "chkISV"
         Me.chkISV.Size = New System.Drawing.Size(83, 21)
         Me.chkISV.TabIndex = 4
         Me.chkISV.Text = "15% ISV"
         Me.chkISV.UseVisualStyleBackColor = True
         '
+        'cbDescuento
+        '
+        Me.cbDescuento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbDescuento.FormattingEnabled = True
+        Me.cbDescuento.Items.AddRange(New Object() {"5%", "10%", "15%", "20%", "25%", "30%"})
+        Me.cbDescuento.Location = New System.Drawing.Point(12, 253)
+        Me.cbDescuento.Name = "cbDescuento"
+        Me.cbDescuento.Size = New System.Drawing.Size(98, 24)
+        Me.cbDescuento.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Subtotal"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtTotal)
+        Me.GroupBox2.Controls.Add(Me.txtISV)
+        Me.GroupBox2.Controls.Add(Me.txtDescuento)
+        Me.GroupBox2.Controls.Add(Me.txtSubtotal)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 297)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(234, 220)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pago"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(84, 176)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(121, 22)
+        Me.txtTotal.TabIndex = 13
+        '
+        'txtISV
+        '
+        Me.txtISV.Location = New System.Drawing.Point(84, 133)
+        Me.txtISV.Name = "txtISV"
+        Me.txtISV.Size = New System.Drawing.Size(121, 22)
+        Me.txtISV.TabIndex = 12
+        '
+        'txtDescuento
+        '
+        Me.txtDescuento.Location = New System.Drawing.Point(84, 90)
+        Me.txtDescuento.Name = "txtDescuento"
+        Me.txtDescuento.Size = New System.Drawing.Size(121, 22)
+        Me.txtDescuento.TabIndex = 11
+        '
+        'txtSubtotal
+        '
+        Me.txtSubtotal.Location = New System.Drawing.Point(84, 38)
+        Me.txtSubtotal.Name = "txtSubtotal"
+        Me.txtSubtotal.Size = New System.Drawing.Size(121, 22)
+        Me.txtSubtotal.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 176)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 17)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Total"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 133)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "15% ISV"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 90)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Descuento"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem, Me.CerrarToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(122, 52)
+        '
+        'NuevoToolStripMenuItem
+        '
+        Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(121, 24)
+        Me.NuevoToolStripMenuItem.Text = "Nuevo"
+        '
+        'CerrarToolStripMenuItem
+        '
+        Me.CerrarToolStripMenuItem.Name = "CerrarToolStripMenuItem"
+        Me.CerrarToolStripMenuItem.Size = New System.Drawing.Size(121, 24)
+        Me.CerrarToolStripMenuItem.Text = "Cerrar"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.SlateGray
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuOpciones})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(378, 28)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuOpciones
+        '
+        Me.MenuOpciones.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuNuevo, Me.menuSalir})
+        Me.MenuOpciones.Name = "MenuOpciones"
+        Me.MenuOpciones.Size = New System.Drawing.Size(85, 24)
+        Me.MenuOpciones.Text = "Opciones"
+        '
+        'menuNuevo
+        '
+        Me.menuNuevo.Name = "menuNuevo"
+        Me.menuNuevo.Size = New System.Drawing.Size(135, 26)
+        Me.menuNuevo.Text = "Nuevo"
+        '
+        'menuSalir
+        '
+        Me.menuSalir.Name = "menuSalir"
+        Me.menuSalir.Size = New System.Drawing.Size(135, 26)
+        Me.menuSalir.Text = "Salir"
+        '
+        'btnPagar
+        '
+        Me.btnPagar.Location = New System.Drawing.Point(279, 149)
+        Me.btnPagar.Name = "btnPagar"
+        Me.btnPagar.Size = New System.Drawing.Size(75, 34)
+        Me.btnPagar.TabIndex = 11
+        Me.btnPagar.Text = "Pagar"
+        Me.btnPagar.UseVisualStyleBackColor = True
+        '
+        'btnPedido
+        '
+        Me.btnPedido.Location = New System.Drawing.Point(270, 54)
+        Me.btnPedido.Name = "btnPedido"
+        Me.btnPedido.Size = New System.Drawing.Size(96, 46)
+        Me.btnPedido.TabIndex = 12
+        Me.btnPedido.Text = "Realizar Pedido"
+        Me.btnPedido.UseVisualStyleBackColor = True
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(378, 450)
+        Me.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.ClientSize = New System.Drawing.Size(378, 540)
+        Me.Controls.Add(Me.btnPedido)
+        Me.Controls.Add(Me.btnPagar)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.cbDescuento)
         Me.Controls.Add(Me.chkISV)
         Me.Controls.Add(Me.chkDescuento)
-        Me.Controls.Add(Me.btnSalir)
         Me.Controls.Add(Me.btnCalcular)
         Me.Controls.Add(Me.GroupBox1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Facturas"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -139,10 +356,32 @@ Partial Class Form1
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents txtCantidad As TextBox
-    Friend WithEvents txtProducto As TextBox
     Friend WithEvents btnCalcular As Button
-    Friend WithEvents btnSalir As Button
     Friend WithEvents chkDescuento As CheckBox
     Friend WithEvents chkISV As CheckBox
+    Friend WithEvents cbProductos As ComboBox
+    Friend WithEvents cbDescuento As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtISV As TextBox
+    Friend WithEvents txtDescuento As TextBox
+    Friend WithEvents txtSubtotal As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents NuevoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerrarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuOpciones As ToolStripMenuItem
+    Friend WithEvents menuNuevo As ToolStripMenuItem
+    Friend WithEvents menuSalir As ToolStripMenuItem
+    Friend WithEvents btnPagar As Button
+    Friend WithEvents btnPedido As Button
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents Fecha As Label
+    Friend WithEvents mkFecha As MaskedTextBox
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents ToolTip1 As ToolTip
 End Class
--- Todo lo visto/Todo lo visto/Form1.vb
+++ Todo lo visto/Todo lo visto/Form1.vb
@@ -1,3 +1,159 @@
-﻿Public Class Form1
+﻿Imports System.ComponentModel
 
+Public Class Form1
+    Dim Precio As Integer
+    Dim Descuento, ISV As Double
+    Dim subtotal, total, descuento1, isv1 As Double
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles menuSalir.Click
+        End
+    End Sub
+
+    Private Sub chkDescuento_CheckedChanged(sender As Object, e As EventArgs) Handles chkDescuento.CheckedChanged
+        If chkDescuento.Checked = True Then
+            cbDescuento.Enabled = True
+        ElseIf chkDescuento.Checked = False Then
+            cbDescuento.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnPedido_Click(sender As Object, e As EventArgs) Handles btnPedido.Click
+        Dim opc As DialogResult
+        opc = MessageBox.Show("¿Desea realizar un pedido?", "Ingresar", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If opc = DialogResult.Yes Then
+            cbDescuento.Enabled = True
+            cbProductos.Enabled = True
+            mkFecha.Enabled = True
+            chkDescuento.Enabled = True
+            chkISV.Enabled = True
+            btnCalcular.Enabled = True
+            btnPagar.Enabled = True
+            menuNuevo.Enabled = True
+            txtCantidad.Enabled = True
+        End If
+        btnPedido.Enabled = False
+    End Sub
+
+    Private Sub cbProductos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbProductos.SelectedIndexChanged
+        If cbProductos.Text = "Camisetas" Then
+            Precio = 150
+        ElseIf cbProductos.Text = "Tenis" Then
+            Precio = 500
+        ElseIf cbProductos.Text = "Jeans" Then
+            Precio = 300
+        ElseIf cbProductos.Text = "Shorts" Then
+            Precio = 200
+        ElseIf cbProductos.Text = "Gorras" Then
+            Precio = 100
+        ElseIf cbProductos.Text = "Perfumes" Then
+            Precio = 800
+        End If
+    End Sub
+
+    Private Sub cbDescuento_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbDescuento.SelectedIndexChanged
+        If cbDescuento.Text = "5%" Then
+            Descuento = 0.05
+        ElseIf cbDescuento.Text = "10%" Then
+            Descuento = 0.1
+        ElseIf cbDescuento.Text = "15%" Then
+            Descuento = 0.15
+        ElseIf cbDescuento.Text = "20%" Then
+            Descuento = 0.2
+        ElseIf cbDescuento.Text = "25%" Then
+            Descuento = 0.25
+        ElseIf cbDescuento.Text = "30%" Then
+            Descuento = 0.3
+        End If
+    End Sub
+
+    Private Sub menuNuevo_Click(sender As Object, e As EventArgs) Handles menuNuevo.Click
+        txtCantidad.Clear()
+        txtSubtotal.Clear()
+        txtTotal.Clear()
+        txtDescuento.Clear()
+        txtISV.Clear()
+        cbProductos.Text = ""
+        cbDescuento.Text = ""
+        mkFecha.Clear()
+        chkDescuento.Checked = False
+        chkISV.Checked = False
+    End Sub
+
+    Private Sub chkISV_CheckedChanged(sender As Object, e As EventArgs) Handles chkISV.CheckedChanged
+        If chkISV.Checked = True Then
+            ISV = 0.15
+        End If
+    End Sub
+
+    Private Sub btnPagar_Click(sender As Object, e As EventArgs) Handles btnPagar.Click
+        MessageBox.Show("Compra exitosa " + mkFecha.Text, "Finalizar", MessageBoxButtons.OK)
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+
+        Try
+            If Me.ValidateChildren And Val(txtCantidad.Text) > 0 Then
+                For i = 1 To Val(txtCantidad.Text) Step 1
+                    subtotal = Val(txtCantidad.Text) * Precio
+                    descuento1 = subtotal * Descuento
+                    isv1 = subtotal * ISV
+                Next
+            Else
+                MessageBox.Show("Revise los datos ingresados", "Error en Cantidad", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+            txtSubtotal.Text = subtotal
+            txtDescuento.Text = descuento1
+            txtISV.Text = isv1
+            txtTotal.Text = subtotal + isv1 - descuento1
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cbDescuento.Enabled = False
+        cbProductos.Enabled = False
+        mkFecha.Enabled = False
+        chkDescuento.Enabled = False
+        chkISV.Enabled = False
+        btnCalcular.Enabled = False
+        btnPagar.Enabled = False
+        txtCantidad.Enabled = False
+        txtSubtotal.Enabled = False
+        txtDescuento.Enabled = False
+        txtISV.Enabled = False
+        txtTotal.Enabled = False
+        menuNuevo.Enabled = False
+    End Sub
+
+    Private Sub mkFecha_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles mkFecha.MaskInputRejected
+        If mkFecha.Text = "" Then
+            MsgBox("Ingrese una fecha", vbInformation)
+        ElseIf Not IsNumeric(mkFecha.Text) Then
+            MsgBox("Las fechas no permiten letras", vbInformation)
+        End If
+    End Sub
+
+    Private Sub txtCantidad_Validating(sender As Object, e As CancelEventArgs) Handles txtCantidad.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider1.SetError(sender, "")
+        Else
+            Me.ErrorProvider1.SetError(sender, "Campo Obligatorio")
+        End If
+    End Sub
+
+    Private Sub txtCantidad_MouseHover(sender As Object, e As EventArgs) Handles txtCantidad.MouseHover
+        ToolTip1.SetToolTip(txtCantidad, "Ingrese una cantidad (Entero)")
+        ToolTip1.ToolTipTitle = "Cantidad"
+        ToolTip1.ToolTipIcon = ToolTipIcon.Info
+    End Sub
+
+    Private Sub mkFecha_MouseHover(sender As Object, e As EventArgs) Handles mkFecha.MouseHover
+        ToolTip1.SetToolTip(mkFecha, "Ingrese una fecha (dd/mm/aaaa)")
+        ToolTip1.ToolTipTitle = "Fecha"
+        ToolTip1.ToolTipIcon = ToolTipIcon.Info
+    End Sub
 End Class
+
+
+
